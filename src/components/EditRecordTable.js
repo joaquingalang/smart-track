6b89a45bd3fde559908ecd966ccadf228b86a648
Adrian Kyle Mariano <167556034+AKSM-TFT@users.noreply.github.com>
2025-07-27 23:37:02 +0800

    Update: Edit Feature
    - Created two new components for the edit feature
    - Create a feature that allows users to update a record

---
+++ src/components/EditRecordTable.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const EditRecordTable = ({ employees, onEditClick }) => {
+  return (
+    <div className="table-wrapper">
+      <div className="table-header">
+        <span>Employee_ID</span>
+        <span>Name</span>
+        <span>Department</span>
+        <span>Position</span>
+        <span></span>
+      </div>
+
+      <div className="table-body">
+        {employees.map((emp) => (
+          <div key={emp.id} className="table-row">
+            <span>{emp.id}</span>
+            <span>{emp.name}</span>
+            <span>{emp.department}</span>
+            <span>{emp.position}</span>
+            <button onClick={() => onEditClick(emp)}>✎</button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default EditRecordTable;
