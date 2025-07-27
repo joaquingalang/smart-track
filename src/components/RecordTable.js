2991cc8a909c843a4101d2f07aab426ac3a5003c
joaquingalang <galangjoaquin@gmail.com>
2025-07-28 00:25:11 +0800

    Add: record table component

--- src/components/RecordTable.js
+++ src/components/RecordTable.js
@@ -1,10 +1,27 @@
+import "../styles/Fonts.css";
 
-function RecordTable() {
-    return (
-        <div>
-            
-        </div>
-    );
-}
+const RecordTable = ({ employees }) => {
+  return (
+    <div className="table-wrapper">
+      <div className="table-header">
+        <span className="delius-regular">Employee_ID</span>
+        <span className="delius-regular">Name</span>
+        <span className="delius-regular">Department</span>
+        <span className="delius-regular">Position</span>
+      </div>
+
+      <div className="table-body-max">
+        {employees.map((emp) => (
+          <div key={emp.id} className="table-row">
+            <span className="delius-regular">{emp.id}</span>
+            <span className="delius-regular">{emp.name}</span>
+            <span className="delius-regular">{emp.department}</span>
+            <span className="delius-regular">{emp.position}</span>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default RecordTable;
