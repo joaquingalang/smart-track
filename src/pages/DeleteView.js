c273227b5f6b3cd6608cd2c2d0779b4c19c08399
Ellaine Gonzales <epgonzales@student.hau.edu.ph>
2025-07-28 15:37:13 +0800

    Delete page WIP

--- src/pages/DeleteView.js
+++ src/pages/DeleteView.js
@@ -1,20 +1,6 @@
-import "../styles/LogoutPage.css"
-import { useNavigate } from "react-router-dom";
-
 function DeleteView() {
     return (
-        <div className="main-container">
-            <div className="headers">
-                <h1 className="logout-header">Are you sure you <br /> want to log out? </h1>
-                <p className="sub-heading">Your current session will end. Any unsaved changes may be lost.</p>
-            </div>
-
-            <div className="buttons">
-                <button className="logout-button">Yes, Log Out</button>
-                <button className="cancel-button">Cancel</button>
-            </div>
-        </div> 
-
+        <div>Delete View</div>
     );
 }
 
--- src/pages/LogoutPageView.js
+++ src/pages/LogoutPageView.js
@@ -1,5 +1,30 @@
+import { useNavigate } from "react-router-dom";
+import "../styles/LogoutPage.css"
+
 function LogoutPageView(){
+    const navigate = useNavigate();
+
+    const handleLogout = () =>{
+        //To add navigation to Landing Page
+    };
+
+    const handleCancel = () => {
+        //To navigate Dashboard with the records
+    };
+
     return(
-    <div>Gay</div>
-);
+    <div className="main-container">
+            <div className="headers">
+                <h1 className="logout-header">Are you sure you <br /> want to log out? </h1>
+                <p className="sub-heading">Your current session will end. Any unsaved changes may be lost.</p>
+            </div>
+
+            <div className="buttons">
+                <button className="logout-button" onClick={handleLogout}>Yes, Log Out</button>
+                <button className="cancel-button" onClick={handleCancel}>Cancel</button>
+            </div>
+        </div>
+    );
 }
+
+export default LogoutPageView;
